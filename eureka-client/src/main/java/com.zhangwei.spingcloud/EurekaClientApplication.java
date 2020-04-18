58bc9dfb97d45ba5b8b5bfe975acd717dab6478e
zhanghengwei <1274051096@qq.com>
2020-04-19 00:05:56 +0800

    cluster分支的创建，配置node1，node2两个节点组成集群

--- eureka-client/src/main/java/com.zhangwei.spingcloud/EurekaClientApplication.java
+++ eureka-client/src/main/java/com.zhangwei.spingcloud/EurekaClientApplication.java
@@ -1,4 +1,4 @@
-package com.zhangwei.springcloud;
+package com.zhangwei.spingcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
